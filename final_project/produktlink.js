56b89c23b5d373b075fd7025ba7910e9f2f4edf1
Chris-pie-Chris <christiant1996@hotmail.com>
2023-12-11 12:49:35 +0100

    A55

---
+++ final_project/produktlink.js
@@ -0,0 +1,8 @@
+function renderProdukt(produkter, index) {
+    `<a href="produkt.html?id=${index}" >${produkter.name}</a>`
+}
+
+const params = new URLSearchParams(location.search)
+
+console.log(params.get("name"))
+console.log(produkter[params.get("name")])
--- final_project/script.js
+++ final_project/script.js
@@ -1,26 +1,8 @@
 // Import af data fra JSON-filen (antager, at './data.json' er det korrekte sti)
-import produkter from './data.json' assert {type: JSON};
-
-// fetch('./data.json')
-//   .then(response => response.json()) // Konverterer svaret til JSON-format
-//   .then(data => { // Bruger dataene, når de er klar
-//     data.forEach(renderProdukt); // Kalder renderProdukt-funktionen for hvert element i data-arrayet
-//   })
-//   .catch(error => { // Håndterer eventuelle fejl under hentningen af dataene
-//     console.error('Fejl ved indlæsning af data:', error);
-//   });
+import produkter from './data.json' assert {type: "json"};
 
 // Loop gennem produkterne og opret HTML-elementer
 produkter.forEach(renderProdukt);
-
-// fetch('./data.json')
-//   .then(response => response.json())
-//   .then(data => {
-//     data.forEach(renderProdukt);
-//   })
-//   .catch(error => {
-//     console.error('Fejl ved indlæsning af data:', error);
-//   });
 
 function renderProdukt(data) {
     let article = document.createElement("article");
@@ -35,8 +17,6 @@ function renderProdukt(data) {
     document.querySelector("#produkter").appendChild(article);
   }
 
-
-
 // function renderProdukt(produkter, index) {
 //     `<a href="produkt.html?id=${index}" >${produkt.name}</a>`
 // }
@@ -45,4 +25,3 @@ function renderProdukt(data) {
 
 // console.log(params.get("id"))
 // console.log(produkter[params.get("id")])
-
